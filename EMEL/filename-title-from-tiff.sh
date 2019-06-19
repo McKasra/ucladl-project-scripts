cc414e97c6795aa332ac39e58e6f241670008551
McKasra <50338667+McKasra@users.noreply.github.com>
2019-06-19 10:07:11 -0700

    Add files via upload
    Bash script to batch extract page titles from TIFF headers with their filenames and put them as columns in a spreadsheet.
    Requirements: ExifTool; connexion to network storage, e.g. NetApp
    Once the scanning of the directory and reading of the image files have been completed, the CSV will be complete with page filenames extracted from the source path of the image file and page titles extracted from the TIFF header. This CSV may be imported into a Google Sheet and/or submitted to the Samvera team.

--- EMEL/filename-title-from-tiff.sh
+++ EMEL/filename-title-from-tiff.sh
@@ -5,25 +5,25 @@ echo Path to MS directory:
 read pathToMSDirectory
 echo Output filename without extension or spaces:
 read pathToCSV
-exiftool -csv -Title $pathToMSDirectory > $pathToCSV".csv"
+exiftool -csv -Title $pathToMSDirectory > $pathToCSV"_temp.csv"
 
 # Sort rows of CSV and save the output to a new/final CSV
-sort -k1 -n -t, $pathToCSV".csv" > $pathToCSV"_final.csv"
+sort -k1 -n -t, $pathToCSV"_temp.csv" > $pathToCSV".csv"
 
 # Delete the first CSV
-rm $pathToCSV".csv"
+rm $pathToCSV"_temp.csv"
 
 # Remove the first 4 rows (the sample shots) from the CSV
-sed -i '' 1,4d $pathToCSV"_final.csv"
+sed -i '' 1,4d $pathToCSV".csv"
 
 # Remove the last row (the header row, now last after sort)
-sed -i '' '$d' $pathToCSV"_final.csv"
+sed -i '' '$d' $pathToCSV".csv"
 
 # Truncate the file path with only filenames left in column A
-sed -i '' 's:^\/.*sld:sld:' $pathToCSV"_final.csv"
+sed -i '' 's:^\/.*sld:sld:' $pathToCSV".csv"
 
-# Remove "MS name" (not the directory name) as recorded in TIFF header, e.g. Sinai Arabic 14, from title in column B and prepend the titles' page counter with "f." standing for "folio" 
+# Remove "MS name" (not the directory name) as recorded in TIFF header, e.g. Sinai Arabic 14, from title in column B and prepend the titles' page counter with "f." standing for "folio"
 echo MS name, as recorded in TIFF header, to be removed:
 read MSname
 sed -i "" "/^[^,]*_f_[^,_]*,/s/,$MSname /,f. /
-    s/,$MSname /,/" $pathToCSV"_final.csv"
+    s/,$MSname /,/" $pathToCSV".csv"
